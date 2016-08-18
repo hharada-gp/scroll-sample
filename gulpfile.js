78828131d472dc891d4fb3c9857aad7cb60e1429
原田陽紗子 <hharada@goodpatch.com>
2016-08-18 18:44:07 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+'use strict';
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
+});
+
+gulp.task('default', function() {
+  browserSync({
+    notify: false,
+    port: 3000,
+    files: ["./public/**/*"],
+  });
+  gulp.watch(['./src/scss/*.scss', './src/scss/**/_*.scss'], ['sass']);
+  gulp.watch(['./src/coffee/*.coffee', './src/coffee/**/_*.coffee'], ['coffee']);
+});
+
+
+gulp.task('sass', function() {
+  gulp.src(['./src/scss/*.scss', './src/scss/**/_*.scss'])
+    .pipe($.plumber())
+    .pipe($.sass())
+    .pipe($.autoprefixer())
+    .pipe($.cssmin())
+    .pipe(gulp.dest('./public/css/'))
+    .on('end', reload);
+});
+
+gulp.task('coffee', function() {
+  gulp.src(['./src/coffee/*.coffee', './src/coffee/**/_*.coffee'])
+    .pipe($.plumber())
+    .pipe($.coffee({
+      bare: true
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('jade', function(){
+  gulp.src(['./src/jade/*.jade', './src/coffee/**/_*.jade'])
+  .pipe($.plumber())
+  .pipe($.jade())
+  .pipe(gulp.dest('./public/'));
+});
